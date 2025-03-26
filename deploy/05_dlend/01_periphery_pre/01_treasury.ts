60318645552c4ff54f29545780e1c04c6b297829
David Zhang <david.d.zhang@gmail.com>
2025-03-26 16:27:20 +0700

    deploy/05_dlend/01_periphery_pre/01_treasury.ts

---
+++ deploy/05_dlend/01_periphery_pre/01_treasury.ts
@@ -0,0 +1,102 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import {
+  TREASURY_CONTROLLER_ID,
+  TREASURY_IMPL_ID,
+  TREASURY_PROXY_ID,
+} from "../../../utils/lending/deploy-ids";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { lendingDeployer, lendingTreasuryOwner } =
+    await hre.getNamedAccounts();
+
+  // Deploy Treasury proxy
+  const treasuryProxyDeployment = await hre.deployments.deploy(
+    TREASURY_PROXY_ID,
+    {
+      from: lendingDeployer,
+      args: [],
+      contract: "InitializableAdminUpgradeabilityProxy",
+      autoMine: true,
+      log: false,
+    }
+  );
+
+  // Deploy Treasury Controller
+  const treasuryControllerDeployment = await hre.deployments.deploy(
+    TREASURY_CONTROLLER_ID,
+    {
+      from: lendingDeployer,
+      args: [lendingTreasuryOwner],
+      contract: "AaveEcosystemReserveController",
+      autoMine: true,
+      log: false,
+    }
+  );
+
+  // Deploy Treasury implementation
+  const treasuryImplDeployment = await hre.deployments.deploy(
+    TREASURY_IMPL_ID,
+    {
+      from: lendingDeployer,
+      args: [],
+      contract: "AaveEcosystemReserveV2",
+      autoMine: true,
+      log: false,
+    }
+  );
+
+  console.log(`-----------------`);
+  console.log(
+    `Initialize AaveEcosystemReserveV2 Impl at ${treasuryImplDeployment.address}`
+  );
+
+  // Initialize implementation contract to prevent other calls
+  const treasuryImplContract = await hre.ethers.getContractAt(
+    "AaveEcosystemReserveV2",
+    treasuryImplDeployment.address
+  );
+
+  // Claim the implementation contract
+  const treasuryImplResponse =
+    await treasuryImplContract.initialize(lendingTreasuryOwner);
+  const treasuryImplReceipt = await treasuryImplResponse.wait();
+  console.log(`  - TxHash: ${treasuryImplReceipt?.hash}`);
+  console.log(`  - From: ${treasuryImplReceipt?.from}`);
+  console.log(`  - GasUsed: ${treasuryImplReceipt?.gasUsed.toString()}`);
+  console.log(`-----------------`);
+
+  // Initialize proxy
+  console.log(
+    `Initialize Treasury InitializableAdminUpgradeabilityProxy at ${treasuryProxyDeployment.address}`
+  );
+  const proxy = await hre.ethers.getContractAt(
+    "InitializableAdminUpgradeabilityProxy",
+    treasuryProxyDeployment.address
+  );
+
+  const initializePayload = treasuryImplContract.interface.encodeFunctionData(
+    "initialize",
+    [treasuryControllerDeployment.address]
+  );
+
+  const initProxyResponse = await proxy["initialize(address,address,bytes)"](
+    treasuryImplDeployment.address,
+    lendingTreasuryOwner,
+    initializePayload
+  );
+  const initProxyReceipt = await initProxyResponse.wait();
+  console.log(`  - TxHash: ${initProxyReceipt?.hash}`);
+  console.log(`  - From: ${initProxyReceipt?.from}`);
+  console.log(`  - GasUsed: ${initProxyReceipt?.gasUsed.toString()}`);
+  console.log(`-----------------`);
+
+  // Return true to indicate deployment success
+  return true;
+};
+
+func.tags = ["lbp", "lbp-periphery-pre", "lbp-TreasuryProxy"];
+func.dependencies = [];
+func.id = "Treasury";
+
+export default func;
