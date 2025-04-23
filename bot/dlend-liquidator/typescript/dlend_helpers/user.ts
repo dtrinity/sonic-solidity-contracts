3427246d5337bf3435d241bb2b0e7e7651f58c88
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-04-24 02:06:31 +0700

    Fixed wrong contract ABI

--- bot/dlend-liquidator/typescript/dlend_helpers/user.ts
+++ bot/dlend-liquidator/typescript/dlend_helpers/user.ts
@@ -47,7 +47,9 @@ export async function getUserAccountData(userAddress: string): Promise<{
 }> {
   const poolContractAddress = await getPoolContractAddress();
   const poolContract = await hre.ethers.getContractAt(
-    "Pool",
+    [
+      "function getUserAccountData(address user) external view returns (uint256 totalCollateralBase, uint256 totalDebtBase, uint256 availableBorrowsBase, uint256 currentLiquidationThreshold, uint256 ltv, uint256 healthFactor)",
+    ],
     poolContractAddress,
   );
   return await poolContract.getUserAccountData(userAddress);
