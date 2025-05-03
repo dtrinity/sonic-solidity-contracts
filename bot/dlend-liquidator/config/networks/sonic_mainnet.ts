9992c7dc96d2184801a403cba8b7af1f0a965ea9
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-05-03 10:42:24 +0800

    Update to use official subgraph URL for Sonic maiinnet

--- bot/dlend-liquidator/config/networks/sonic_mainnet.ts
+++ bot/dlend-liquidator/config/networks/sonic_mainnet.ts
@@ -29,7 +29,7 @@ export async function getConfig(): Promise<Config> {
       profitableThresholdInUSD: 0.001,
       liquidatingBatchSize: 200,
       graphConfig: {
-        url: "http://35.83.220.110/subgraphs/name/stablyio-aave-v3-messari-sonic-mainnet", // TODO: Replace with actual subgraph URL
+        url: "https://graph-node-sonic.dtrinity.org/subgraphs/name/dtrinity-aave-sonic",
         batchSize: 1000,
       },
       isUnstakeTokens: {
