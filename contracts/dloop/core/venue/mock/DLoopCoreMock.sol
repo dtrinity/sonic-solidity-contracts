a54070d8b351b39a4b2863ceeb92df251c49cc9f
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-06-03 03:16:33 +0700

    Implemented test for DLoopCoreMock.sol

--- contracts/dloop/core/venue/mock/DLoopCoreMock.sol
+++ contracts/dloop/core/venue/mock/DLoopCoreMock.sol
@@ -17,6 +17,7 @@ contract DLoopCoreMock is DLoopCoreBase {
     mapping(address => address[]) private mockCollateralTokens; // user => tokens
     mapping(address => mapping(address => uint256)) private mockDebt; // user => token => amount
     mapping(address => address[]) private mockDebtTokens; // user => tokens
+    mapping(address => uint256) private mockPoolBalances; // token => amount (tracked separately from real balances)
     address public mockPool;
 
     uint8 public constant PRICE_DECIMALS = 8;
@@ -53,12 +54,17 @@ contract DLoopCoreMock is DLoopCoreBase {
         mockPrices[asset] = price;
     }
 
+    // Allow setting mock pool balances
+    function setMockPoolBalance(address token, uint256 amount) external {
+        mockPoolBalances[token] = amount;
+    }
+
+    function getMockPoolBalance(address token) external view returns (uint256) {
+        return mockPoolBalances[token];
+    }
+
     // Allow setting mock collateral and debt for a user
-    function setMockCollateral(
-        address user,
-        address token,
-        uint256 amount
-    ) external {
+    function setMockCollateral(address user, address token, uint256 amount) external {
         _setMockCollateral(user, token, amount);
     }
     function _setMockCollateral(
@@ -66,18 +72,21 @@ contract DLoopCoreMock is DLoopCoreBase {
         address token,
         uint256 amount
     ) internal {
-        if (mockCollateral[user][token] == 0) {
+        if (mockCollateral[user][token] == 0 && amount > 0) {
             mockCollateralTokens[user].push(token);
         }
         mockCollateral[user][token] = amount;
-
-        // If new collateral is less than debt, revert
-        (
-            uint256 totalCollateralInBase,
-            uint256 totalDebtInBase
-        ) = getTotalCollateralAndDebtOfUserInBase(user);
-        if (totalCollateralInBase < totalDebtInBase) {
-            revert("Mock: collateral is less than debt");
+        
+        // Remove token from array if amount becomes 0
+        if (amount == 0) {
+            for (uint256 i = 0; i < mockCollateralTokens[user].length; i++) {
+                if (mockCollateralTokens[user][i] == token) {
+                    // Replace with last element and pop
+                    mockCollateralTokens[user][i] = mockCollateralTokens[user][mockCollateralTokens[user].length - 1];
+                    mockCollateralTokens[user].pop();
+                    break;
+                }
+            }
         }
     }
 
@@ -89,18 +98,21 @@ contract DLoopCoreMock is DLoopCoreBase {
         address token,
         uint256 amount
     ) internal {
-        if (mockDebt[user][token] == 0) {
+        if (mockDebt[user][token] == 0 && amount > 0) {
             mockDebtTokens[user].push(token);
         }
         mockDebt[user][token] = amount;
-
-        // If new debt is greater than collateral, revert
-        (
-            uint256 totalCollateralInBase,
-            uint256 totalDebtInBase
-        ) = getTotalCollateralAndDebtOfUserInBase(user);
-        if (totalCollateralInBase < totalDebtInBase) {
-            revert("Mock: collateral is less than debt");
+        
+        // Remove token from array if amount becomes 0
+        if (amount == 0) {
+            for (uint256 i = 0; i < mockDebtTokens[user].length; i++) {
+                if (mockDebtTokens[user][i] == token) {
+                    // Replace with last element and pop
+                    mockDebtTokens[user][i] = mockDebtTokens[user][mockDebtTokens[user].length - 1];
+                    mockDebtTokens[user].pop();
+                    break;
+                }
+            }
         }
     }
 
@@ -154,16 +166,23 @@ contract DLoopCoreMock is DLoopCoreBase {
         uint256 amount,
         address onBehalfOf
     ) internal override {
-        // Mimic: increase debt for onBehalfOf, transfer token from pool to onBehalfOf
-        _setMockDebt(onBehalfOf, token, mockDebt[onBehalfOf][token] + amount);
+        // Check mock pool balance (separate from real token balance)
         require(
-            ERC20(token).balanceOf(mockPool) >= amount,
+            mockPoolBalances[token] >= amount,
             "Mock: not enough tokens in pool to borrow"
         );
+        
+        // Transfer from vault to user (simulating pool lending)
         require(
-            ERC20(token).transferFrom(mockPool, onBehalfOf, amount),
+            ERC20(token).transfer(onBehalfOf, amount),
             "Mock: borrow transfer failed"
         );
+        
+        // Decrease mock pool balance to simulate pool lending
+        mockPoolBalances[token] -= amount;
+        
+        // Set debt after successful transfer
+        _setMockDebt(onBehalfOf, token, mockDebt[onBehalfOf][token] + amount);
     }
 
     function _repayDebtToPoolImplementation(
@@ -206,8 +225,9 @@ contract DLoopCoreMock is DLoopCoreBase {
             ERC20(token).balanceOf(mockPool) >= amount,
             "Mock: not enough tokens in pool to withdraw"
         );
+        // Transfer from vault (this contract) to onBehalfOf, simulating pool behavior
         require(
-            ERC20(token).transferFrom(mockPool, onBehalfOf, amount),
+            ERC20(token).transfer(onBehalfOf, amount),
             "Mock: withdraw transfer failed"
         );
     }
@@ -230,6 +250,7 @@ contract DLoopCoreMock is DLoopCoreBase {
 
             // Convert collateral to base unit
             uint256 price = mockPrices[token];
+            require(price > 0, "Mock price not set");
             uint256 amount = mockCollateral[user][token];
             uint256 assetTokenUnit = 10 ** ERC20(token).decimals();
             uint256 amountInBase = (amount * price) / assetTokenUnit;
@@ -241,6 +262,7 @@ contract DLoopCoreMock is DLoopCoreBase {
 
             // Convert debt to base unit
             uint256 price = mockPrices[token];
+            require(price > 0, "Mock price not set");
             uint256 amount = mockDebt[user][token];
             uint256 assetTokenUnit = 10 ** ERC20(token).decimals();
             uint256 amountInBase = (amount * price) / assetTokenUnit;
@@ -282,5 +304,186 @@ contract DLoopCoreMock is DLoopCoreBase {
         address onBehalfOf
     ) external {
         _withdrawFromPool(token, amount, onBehalfOf);
+    }
+
+    // --- Additional Test Wrappers for Internal Methods ---
+
+    /**
+     * @dev Test wrapper for _getAdditionalRescueTokensImplementation
+     */
+    function testGetAdditionalRescueTokens() external pure returns (address[] memory) {
+        return _getAdditionalRescueTokensImplementation();
+    }
+
+    /**
+     * @dev Test wrapper for _getAssetPriceFromOracleImplementation
+     */
+    function testGetAssetPriceFromOracle(address asset) external view returns (uint256) {
+        return _getAssetPriceFromOracleImplementation(asset);
+    }
+
+    /**
+     * @dev Test wrapper for _supplyToPoolImplementation
+     */
+    function testSupplyToPoolImplementation(
+        address token,
+        uint256 amount,
+        address onBehalfOf
+    ) external {
+        _supplyToPoolImplementation(token, amount, onBehalfOf);
+    }
+
+    /**
+     * @dev Test wrapper for _borrowFromPoolImplementation
+     */
+    function testBorrowFromPoolImplementation(
+        address token,
+        uint256 amount,
+        address onBehalfOf
+    ) external {
+        _borrowFromPoolImplementation(token, amount, onBehalfOf);
+    }
+
+    /**
+     * @dev Test wrapper for _repayDebtToPoolImplementation
+     */
+    function testRepayDebtToPoolImplementation(
+        address token,
+        uint256 amount,
+        address onBehalfOf
+    ) external {
+        _repayDebtToPoolImplementation(token, amount, onBehalfOf);
+    }
+
+    /**
+     * @dev Test wrapper for _withdrawFromPoolImplementation
+     */
+    function testWithdrawFromPoolImplementation(
+        address token,
+        uint256 amount,
+        address onBehalfOf
+    ) external {
+        _withdrawFromPoolImplementation(token, amount, onBehalfOf);
+    }
+
+    /**
+     * @dev Test wrapper for convertFromBaseCurrencyToToken
+     */
+    function testConvertFromBaseCurrencyToToken(
+        uint256 amountInBase,
+        address token
+    ) external view returns (uint256) {
+        return convertFromBaseCurrencyToToken(amountInBase, token);
+    }
+
+    /**
+     * @dev Test wrapper for convertFromTokenAmountToBaseCurrency
+     */
+    function testConvertFromTokenAmountToBaseCurrency(
+        uint256 amountInToken,
+        address token
+    ) external view returns (uint256) {
+        return convertFromTokenAmountToBaseCurrency(amountInToken, token);
+    }
+
+    /**
+     * @dev Test wrapper for getBorrowAmountThatKeepCurrentLeverage
+     */
+    function testGetBorrowAmountThatKeepCurrentLeverage(
+        address collateralAsset,
+        address debtAsset,
+        uint256 suppliedCollateralAmount,
+        uint256 leverageBpsBeforeSupply
+    ) external view returns (uint256) {
+        return getBorrowAmountThatKeepCurrentLeverage(
+            collateralAsset,
+            debtAsset,
+            suppliedCollateralAmount,
+            leverageBpsBeforeSupply
+        );
+    }
+
+    /**
+     * @dev Test wrapper for getRepayAmountThatKeepCurrentLeverage
+     */
+    function testGetRepayAmountThatKeepCurrentLeverage(
+        address collateralAsset,
+        address debtAsset,
+        uint256 targetWithdrawAmount,
+        uint256 leverageBpsBeforeRepayDebt
+    ) external view returns (uint256) {
+        return getRepayAmountThatKeepCurrentLeverage(
+            collateralAsset,
+            debtAsset,
+            targetWithdrawAmount,
+            leverageBpsBeforeRepayDebt
+        );
+    }
+
+    /**
+     * @dev Test wrapper for getAmountToReachTargetLeverage
+     */
+    function testGetAmountToReachTargetLeverage(
+        bool useVaultTokenBalance
+    ) external view returns (uint256 tokenAmount, int8 direction) {
+        return getAmountToReachTargetLeverage(useVaultTokenBalance);
+    }
+
+    /**
+     * @dev Test wrapper for isTooImbalanced
+     */
+    function testIsTooImbalanced() external view returns (bool) {
+        return isTooImbalanced();
+    }
+
+    /**
+     * @dev Test wrapper for getLeveragedAssets
+     */
+    function testGetLeveragedAssets(uint256 assets) external view returns (uint256) {
+        return getLeveragedAssets(assets);
+    }
+
+    /**
+     * @dev Test wrapper for getRestrictedRescueTokens
+     */
+    function testGetRestrictedRescueTokens() external view returns (address[] memory) {
+        return getRestrictedRescueTokens();
+    }
+
+    // --- Mock State Getters for Testing ---
+
+    /**
+     * @dev Get mock collateral for a user and token
+     */
+    function getMockCollateral(address user, address token) external view returns (uint256) {
+        return mockCollateral[user][token];
+    }
+
+    /**
+     * @dev Get mock debt for a user and token
+     */
+    function getMockDebt(address user, address token) external view returns (uint256) {
+        return mockDebt[user][token];
+    }
+
+    /**
+     * @dev Get all collateral tokens for a user
+     */
+    function getMockCollateralTokens(address user) external view returns (address[] memory) {
+        return mockCollateralTokens[user];
+    }
+
+    /**
+     * @dev Get all debt tokens for a user
+     */
+    function getMockDebtTokens(address user) external view returns (address[] memory) {
+        return mockDebtTokens[user];
+    }
+
+    /**
+     * @dev Get mock price for an asset
+     */
+    function getMockPrice(address asset) external view returns (uint256) {
+        return mockPrices[asset];
     }
 }
