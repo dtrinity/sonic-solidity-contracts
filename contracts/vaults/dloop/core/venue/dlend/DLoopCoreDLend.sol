edd6534ea9a7ada30cc030895593c284840acb83
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-08-17 02:21:02 +0700

    Removed unused import

--- contracts/vaults/dloop/core/venue/dlend/DLoopCoreDLend.sol
+++ contracts/vaults/dloop/core/venue/dlend/DLoopCoreDLend.sol
@@ -21,7 +21,6 @@ import {IPriceOracleGetter} from "./interface/IPriceOracleGetter.sol";
 import {IPool as ILendingPool, DataTypes} from "./interface/IPool.sol";
 import {IPoolAddressesProvider} from "./interface/IPoolAddressesProvider.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {DLoopCoreBase} from "../../DLoopCoreBase.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
