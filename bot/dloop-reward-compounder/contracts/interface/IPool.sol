dc1516d6b34fbdfd6cb46d892bdfefc8f2eea45f
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-08-20 20:25:26 +0700

    Revert "Commmit first version of dloop-reward-compounder"
    This reverts commit 67da2948787324c8c485ed13abfc7ab6a8ed7196.

--- bot/dloop-reward-compounder/contracts/interface/IPool.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IPool {
-    function getReservesList() external view returns (address[] memory);
-}
-
--- bot/dloop-reward-compounder/contracts/interface/IPoolAddressesProvider.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IPoolAddressesProvider {
-    function getPool() external view returns (address);
-}
-
