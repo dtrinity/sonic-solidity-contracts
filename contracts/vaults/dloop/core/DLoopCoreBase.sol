9b90a872bfbf57532f564690e0563881628df9e2
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2025-08-26 00:42:43 +0700

    Migrated rebalancing quote logic to contracts/vaults/dloop/core/DLoopQuoter.sol

--- contracts/vaults/dloop/core/DLoopCoreBase.sol
+++ contracts/vaults/dloop/core/DLoopCoreBase.sol
@@ -896,39 +896,6 @@ abstract contract DLoopCoreBase is ERC4626, Ownable, ReentrancyGuard, RescuableV
     /* Rebalance */
 
     /**
-     * @notice Gets the rebalance amount to reach the target leverage in token units
-     * @dev This method is used by rebalancing services to quote required collateral/debt amounts
-     *      and determine the rebalancing direction (increase or decrease leverage)
-     * @return inputTokenAmount The amount of token to call increaseLeverage or decreaseLeverage (in token unit)
-     *         - If direction is 1, the amount is in collateral token
-     *         - If direction is -1, the amount is in debt token
-     * @return estimatedOutputTokenAmount The estimated output token amount after the rebalance (in token unit)
-     *         - If direction is 1, the amount is in debt token
-     *         - If direction is -1, the amount is in collateral token
-     * @return direction The direction of the rebalance (1 for increase, -1 for decrease, 0 means no rebalance)
-     */
-    function quoteRebalanceAmountToReachTargetLeverage()
-        public
-        view
-        returns (uint256 inputTokenAmount, uint256 estimatedOutputTokenAmount, int8 direction)
-    {
-        (uint256 totalCollateralBase, uint256 totalDebtBase) = getTotalCollateralAndDebtOfUserInBase(address(this));
-
-        return
-            DLoopCoreLogic.quoteRebalanceAmountToReachTargetLeverage(
-                totalCollateralBase,
-                totalDebtBase,
-                getCurrentLeverageBps(),
-                targetLeverageBps,
-                getCurrentSubsidyBps(),
-                ERC20(collateralToken).decimals(),
-                getAssetPriceFromOracle(address(collateralToken)),
-                ERC20(debtToken).decimals(),
-                getAssetPriceFromOracle(address(debtToken))
-            );
-    }
-
-    /**
      * @notice Increases the leverage of the user by supplying collateral token and borrowing more debt token
      * @dev Requires spending collateral token from the user's wallet to supply to the pool.
      *      Will send the borrowed debt token to the user's wallet.
